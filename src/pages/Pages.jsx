5992d7275513391a02f7f18e8613cf1145ea3da0
Jose Ruiz <joeruizl87@gmail.com>
2023-07-12 13:13:15 -0400

    additional pages

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,10 +1,24 @@
 import Home from "./Home";
-
-import React from 'react'
+import React from 'react';
+import Cuisine from "./Cuisine";
+import Searched from "./Searched";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Recipe from "./Recipe";
+import { AnimatePresence } from "framer-motion";
 
 function Pages() {
+
+    const location = useLocation();
+
     return (
-        <Home />
+        <AnimatePresence mode="wait">
+            <Routes Location={location} key={location.pathname}>
+                <Route path="/" element={<Home />} />
+                <Route path="/cuisine/:type" element={<Cuisine />} />
+                <Route path="/searched/:search" element={<Searched />} />
+                <Route path="/recipe/:name" element={<Recipe />} />
+            </Routes>
+        </AnimatePresence>
     )
 }
 
